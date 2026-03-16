3e74a8015c09c8e1716579fa658694932c2bf2dc
Palani <palaniappan.murugappan@gmail.com>
2026-03-16 17:07:39 +0400

    setup file created

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Source Code Analyser',
+    author='Palani',
+    author_email="palaniappan.murugappan@gmail.com",
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[]
+)
